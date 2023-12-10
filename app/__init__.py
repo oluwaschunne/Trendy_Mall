d411008e858f3ecbb22dcf8b53289b5ae7724bff
oluwaschunne <falopeoluwaseun2@gmail.com>
2023-12-10 08:09:48 +0100

    Trendy

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,4 @@ from config import Config
 app = Flask(__name__)
 app.config.from_object(Config)
 
-@app.route('/')
-def home():
-    return "Incoming Trendy_Mall!!!"
-
+from app.routes.root import *
